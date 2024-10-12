43c838f5a88f7299e87c419b1dcc122e08690d99
praditaarif <praditaarif9@gmail.com>
2024-10-12 22:03:24 +0700

    datamaster-obat creat clear

--- src/datamaster-obat/dataobat.php
+++ src/datamaster-obat/dataobat.php
@@ -20,85 +20,15 @@
 </head>
 
 <body class="bg-green-100 text-gray-900">
-
-    <!-- Sidebar -->
-    <aside class="w-64 h-screen bg-green-600 fixed top-0 left-0 p-6 shadow-lg">
-        <h1 class="text-3xl font-bold mb-8 text-white">Apoteku</h1>
-
-        <nav>
-            <ul>
-                <!-- Beranda -->
-                <li class="mb-6 list-none">
-                    <a href="index.php"
-                        class="w-full bg-green-700 text-white p-3 rounded-lg flex items-center justify-center hover:bg-green-800">
-                        <span>Beranda</span>
-                    </a>
-                </li>
-
-                <!-- Transaksi Section -->
-                <div class="mt-8">
-                    <button onclick="toggleDropdown('transaksi-dropdown')"
-                        class="w-full bg-green-800 text-white p-3 rounded-lg flex items-center justify-center hover:bg-green-800 transition duration-300">
-                        <span>Transaksi</span>
-                    </button>
-                </div>
-
-                <!-- Dropdown Menu -->
-                <div id="transaksi-dropdown" class="hidden mt-4">
-                    <!-- Data penjualan -->
-                    <li class="mb-4 list-none">
-                        <a href="transaksi-penjualan.php"
-                            class="text-white bg-green-700 p-3 rounded-lg flex items-center hover:bg-green-800 transition duration-300">
-                            <span>Penjualan</span>
-                        </a>
-                    </li>
-
-                    <!-- Data pembelian -->
-                    <li class="list-none">
-                        <a href="datasuplier.php"
-                            class="text-white p-3 rounded-lg flex items-center hover:bg-green-700 transition duration-300">
-                            <span>Pembelian</span>
-                        </a>
-                    </li>
-                </div>
-
-                <!-- Data Master Section -->
-                <div class="mt-8">
-                    <button onclick="toggleDropdown('master-dropdown')"
-                        class="w-full bg-green-700 text-white p-3 rounded-lg flex items-center justify-center hover:bg-green-800 transition duration-300">
-                        <span>Data Master</span>
-                    </button>
-                </div>
-
-                <!-- Dropdown Menu -->
-                <div id="master-dropdown" class="hidden mt-4">
-                    <!-- Data Obat -->
-                    <li class="mb-4 list-none">
-                        <a href="dataobat.php"
-                            class="text-white p-3 rounded-lg flex items-center hover:bg-green-700 transition duration-300">
-                            <span>Data Obat</span>
-                        </a>
-                    </li>
-
-                    <!-- Data Suplier -->
-                    <li class="list-none">
-                        <a href="datasuplier.php"
-                            class="text-white p-3 rounded-lg flex items-center hover:bg-green-700 transition duration-300">
-                            <span>Data Suplier</span>
-                        </a>
-                    </li>
-                </div>
-            </ul>
-        </nav>
-    </aside>
-
+<?php include('../template/sidebar.php'); ?>
+  
     <!-- Container utama dengan margin kiri untuk menghindari tumpang tindih dengan sidebar -->
-    <div class="ml-64 max-w-6xl mx-auto mt-10 p-6">
+    <div class="ml-64 max-w-6xl mx-auto p-6">
         <!-- Header -->
         <h1 class="text-2xl font-bold mb-4">Data Obat</h1>
 
         <!-- Tombol Tambah Data -->
-        <a href="/apoteku/apoteku/src/dataobat-create.php"class="bg-green-800 text-white p-3 rounded-lg flex items-center justify-center hover:bg-green-800 mb-6">Tambah Data</a>
+        <a href="dataobat-create.php"class="bg-green-800 text-white p-3 rounded-lg flex items-center justify-center hover:bg-green-800 mb-6">Tambah Data</a>
 
         <!-- Tabel -->
         <div class=" bg-white shadow-md rounded-lg overflow-x-auto w-full">
