1c86f1b4e4df411454a3072e4ebabf69200780de
praditaarif <praditaarif9@gmail.com>
2024-10-13 21:14:57 +0700

    create supplier sudah

--- src/datamaster-supplier/delete-supplier.php
+++ src/datamaster-supplier/delete-supplier.php
@@ -2,6 +2,7 @@
 if (isset($_GET['id'])) {
     include('../database/database.php');
     $id = $_GET['id'];
+    $sql = "DELETE FROM supplier WHERE ID_Supplier = $id";
     if (mysqli_query($conn, $sql)) {
         header('Location: index-supplier.php');
     } else {
